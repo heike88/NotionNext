812598c12d5ac649e19ccfa8083e95cb189298db
peter <98251349+heike88@users.noreply.github.com>
2023-02-25 12:31:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   AUTHOR: 'zhoujc', // 作者
-  BIO: '万丈红尘三杯酒', // 作者简介
+  BIO: 'coding~~', // 作者简介
   LINK: 'https://zhoujc.top', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
